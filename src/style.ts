0ae46d284d8b817e7e7b543053bcb50bc2de3c7e
Josh Marinacci <joshua@marinacci>
2022-10-09 09:56:19 -0700

    add default style

--- src/style.ts
+++ src/style.ts
@@ -3,13 +3,25 @@ import {BElement, BInsets, BStyleSet, BText} from "./common";
 import ohm from "ohm-js";
 
 
+const default_stylesheet = String.raw`
+* {
+    font-size: 10pt;
+    display:block;
+    color:black;
+    background-color:white;
+    padding: 0;
+    margin: 0;
+    border: 1px solid black;
+}
+`
+
 const raw_grammar = String.raw`
 CSS {
     RuleSet = Rule*
     Rule = ident "{" RuleBody "}"
     RuleBody = RuleItem*
     RuleItem = PropName ":" PropValue ";"
-    ident = letter (letter | digit)*
+    ident = ("*" | letter | digit)+
     PropName = letter (letter | "-")*
     PropValue = (~";" any)+
 }
@@ -48,72 +60,67 @@ type CSSProp = {
     value:string,
 }
 
-const default_style = {
-    'background-color':'white',
-    'color':'black',
+let grammar = ohm.grammar(raw_grammar)
+let semantics = grammar.createSemantics()
+// console.log("body_style is",body_style['background-color'])
+semantics.addOperation('rules', {
+    _terminal() {
+        return this.sourceString
+    },
+    _iter: (...children) => children.map(c => c.rules()),
+    ident: (b) => b.rules().join(""),
+    Rule:(selector,_1,body,_2) => {
+        let r:CSSRule = {
+            selector:selector.rules(),
+            props:body.rules(),
+        }
+        return r
+    },
+    PropName:(a,b) => a.rules() + b.rules().join(""),
+    PropValue:(a) => a.rules().join(""),
+    RuleItem:(name,_1,value,_2) => {
+        console.log("property name",name.rules())
+        console.log("property value",value.rules())
+        let prop:CSSProp = {
+            name:name.rules(),
+            value:value.rules(),
+        }
+        return prop
+    }
+})
+
+function parse_style_block(input: string, styles: BStyleSet) {
+    console.log("parsing_style",input)
+    let res1 = grammar.match(input)
+    console.log("passed?",res1.succeeded())
+    if (res1.failed()) throw new Error("match failed")
+    let rules:CSSRule[] = semantics(res1).rules()
+    console.log("rules are",rules)
+    let record:Record<string, string> = {}
+    rules.forEach(rule => {
+        console.log("selector",rule.selector,rule.props)
+        rule.props.forEach(prop => {
+            record[prop.name] = prop.value
+        })
+    })
+    console.log("record is",record)
+    // change styles to cascade
+    // request style for p should return a style object for p that falls back to the style object for body and then to html and then to the default
+    // css properties can inherit from the parent
+    // others can inherit from the generic element
+    // elements inherit from generic element
+    // if element doesn't have a property at all, then get it from the parent
+    // BlockStyle represents the fully calculated styles
 }
-// const body_style = {
-//     'color':'pink',
-//     __proto__:default_style,
-// }
 
 // css string to CSS tree
-export function parse_styles(_styles: string[]): BStyleSet {
-    let grammar = ohm.grammar(raw_grammar)
-    let semantics = grammar.createSemantics()
-    // console.log("body_style is",body_style['background-color'])
-    semantics.addOperation('rules', {
-        _terminal() {
-            return this.sourceString
-        },
-        _iter: (...children) => children.map(c => c.rules()),
-        ident: (a, b) => a.rules() + b.rules().join(""),
-        Rule:(selector,_1,body,_2) => {
-            let r:CSSRule = {
-                selector:selector.rules(),
-                props:body.rules(),
-            }
-            return r
-        },
-        PropName:(a,b) => a.rules() + b.rules().join(""),
-        PropValue:(a) => a.rules().join(""),
-        RuleItem:(name,_1,value,_2) => {
-            console.log("property name",name.rules())
-            console.log("property value",value.rules())
-            let prop:CSSProp = {
-                name:name.rules(),
-                value:value.rules(),
-            }
-            return prop
-        }
-    })
+export function parse_styles(styles: string[]): BStyleSet {
 
-        let styles = new BStyleSet()
-    _styles.forEach(input => {
-        console.log("parsing_style",input)
-        let res1 = grammar.match(input)
-        console.log("passed?",res1.succeeded())
-        if (res1.failed()) throw new Error("match failed")
-        let rules:CSSRule[] = semantics(res1).rules()
-        console.log("rules are",rules)
-        let record:Record<string, string> = {}
-        rules.forEach(rule => {
-            console.log("selector",rule.selector,rule.props)
-            rule.props.forEach(prop => {
-                record[prop.name] = prop.value
-            })
-        })
-        console.log("record is",record)
-        // change styles to cascade
-        // request style for p should return a style object for p that falls back to the style object for body and then to html and then to the default
-        // css properties can inherit from the parent
-        // others can inherit from the generic element
-        // elements inherit from generic element
-        // if element doesn't have a property at all, then get it from the parent
-        // BlockStyle represents the fully calculated styles
-    })
+    let style_set = new BStyleSet()
+    parse_style_block(default_stylesheet,style_set)
+    styles.forEach(input => parse_style_block(input,style_set))
 
-    styles.block.set("html", {
+    style_set.block.set("html", {
         background: 'green',
         border: {
             color: 'black',
@@ -122,7 +129,7 @@ export function parse_styles(_styles: string[]): BStyleSet {
         padding: BInsets.uniform(10),
         margin: BInsets.uniform(0),
     })
-    styles.block.set('p', {
+    style_set.block.set('p', {
         background: 'white',
         border: {
             color: 'black',
@@ -131,9 +138,9 @@ export function parse_styles(_styles: string[]): BStyleSet {
         padding: BInsets.uniform(10),
         margin: BInsets.uniform(0),
     })
-    styles.text.set('p', {
+    style_set.text.set('p', {
         color: 'black',
         fontSize: 15,
     })
-    return styles
+    return style_set
 }
